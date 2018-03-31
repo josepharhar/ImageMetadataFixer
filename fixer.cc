155c30d09b7d5258ca74c3fd0fc339867a72e079
Joseph Arhar <josepharhar@gmail.com>
2018-03-30 19:57:05 -0700

    got exiv to work on linux

--- fixer.cc
+++ fixer.cc
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include <exiv2/exiv2.hpp>
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     printf("usage: %s <image>\n", argv[0]);
--- makefile
+++ makefile
@@ -1,2 +1,8 @@
+FLAGS = -std=c++11
+
 fixer: fixer.cc
-	g++ $< -o $@
+	g++ $(FLAGS) $< -o $@
+
+.PHONY: clean
+clean:
+	-rm -rf fixer
