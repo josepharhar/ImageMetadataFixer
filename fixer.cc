91d244174084c1de3f093874d15cc72015c6aa80
Joseph Arhar <josepharhar@gmail.com>
2018-04-02 12:59:31 -0700

    made lists of to fix files

--- fixer.cc
+++ fixer.cc
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <iostream>
-
 #include <exiv2/exiv2.hpp>
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: %s <image>\n", argv[0]);
+  if (argc != 3 || argc != 4) {
+    printf("usage: %s <image> <offset> [commit]\n", argv[0]);
     return 0;
+  }
+  int offset = atoi(argv[2]);
+  bool commit = false;
+  if (argc > 3 && !strcmp(argv[3], "commit")) {
+    commit = true;
   }
 
   Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(argv[1]);
@@ -23,19 +27,6 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  /*for (Exiv2::ExifData::const_iterator it = exif_data.begin(); it != exif_data.end(); it++) {
-    if (it->key() == "Exif.Image.DateTime"
-        || it->key() == "Exif.Photo.DateTimeOriginal"
-        || it->key() == "Exif.Photo.DateTimeDigitized") {
-      printf("%30s %s\n", it->key().c_str(), it->value().c_str());
-      const char* type_name = it->typeName();
-      std::cout << it->key();
-      printf(" 0x%lX %s %d", it->tag(), type_name ? type_name : "Unknown", it->count());
-      std::cout << it->value();
-      printf("\n");
-    }
-  }*/
-
   const char* tag_names[] = {
     "Exif.Image.DateTime",
     "Exif.Photo.DateTimeOriginal",
@@ -45,21 +36,28 @@ int main(int argc, char** argv) {
     const char* tag_name = tag_names[i];
     Exiv2::Exifdatum& tag = exif_data[tag_name];
     std::string date = tag.toString();
-    printf("%30s tostring: %s\n", tag_name, date.c_str());
+    std::string original_date = date;
 
     const char date_format[] = "%04u:%02u:%02u %02u:%02u:%02u";
     unsigned year = 0, month = 0, day = 0, hours = 0, minutes = 0, seconds = 0;
     sscanf(date.c_str(), date_format, &year, &month, &day, &hours, &minutes, &seconds);
-    //printf("  year: %u, month: %u, day: %u, hours: %u, minutes: %u, seconds: %u\n", year, month, day, hours, minutes, seconds);
 
-    hours -= 2;
+    if (((int)hours) + offset < 0) {
+      printf("HOURS UNDERFLOW!!!\n");
+    } else if (hours + offset >= 24) {
+      printf("HOURS OVERFLOW!!\n");
+    } else {
+      hours += offset;
+    }
 
-    //printf("  year: %u, month: %u, day: %u, hours: %u, minutes: %u, seconds: %u\n", year, month, day, hours, minutes, seconds);
     sprintf((char*)date.c_str(), date_format, year, month, day, hours, minutes, seconds);
 
-    printf("%30s      new: %s\n", tag_name, date.c_str());
+    printf("%30s old: %s\n", tag_name, original_date.c_str());
+    printf("%30s new: %s\n", tag_name, date.c_str());
 
-    tag.setValue(date);
+    if (commit) {
+      tag.setValue(date);
+    }
   }
 
   image->setExifData(exif_data);
